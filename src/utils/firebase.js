0f3f016bbc98a446a389864f33c938ed351615f6
Ritesh Gite <riteshgite2005@gmail.com>
2025-11-29 11:17:04 +0530

    Final cleanup for deployment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDwvuriw74achqgIKJLUAjWsanHumMo7-M",
-    authDomain: "netflixgpt-704b4.firebaseapp.com",
-    projectId: "netflixgpt-704b4",
-    storageBucket: "netflixgpt-704b4.firebasestorage.app",
-    messagingSenderId: "863746394512",
-    appId: "1:863746394512:web:cd47f586e246dd93a54b15",
-    measurementId: "G-NXQ1REF05V"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
